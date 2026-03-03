ff0c387ce54634f0b0f064c7ad50211bde4f99b3
Shakir Bhat <bhatshakir16@gmail.com>
2026-03-03 08:21:53 +0530

    video compressed and code optimized

--- src/app/shared/components/product-card.ts
+++ src/app/shared/components/product-card.ts
@@ -14,8 +14,11 @@ import { RouterLink } from '@angular/router';
           [src]="product().images[0]"
           [alt]="product().name"
           class="w-full h-full object-cover transition-transform duration-700 group-hover:scale-105"
+          loading="lazy"
+          decoding="async"
           referrerPolicy="no-referrer"
         />
+
         <div class="absolute inset-0 bg-black/0 group-hover:bg-black/5 transition-colors duration-300"></div>
       </div>
       

ae4aef9948d5f59ba027118036a1e031ba59082d
Shakir Bhat <bhatshakir16@gmail.com>
2026-03-15 13:38:37 +0530

    feat: implement internationalization with Transloco, adding translation files for multiple languages and configuring the loader. first Commit - i18n

--- projects/customer-app/src/app/app.config.ts
+++ projects/customer-app/src/app/app.config.ts
@@ -5,9 +5,12 @@ import {
 import { provideRouter, withComponentInputBinding, withViewTransitions } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideTransloco } from '@jsverse/transloco';
 
 import { routes } from './app.routes';
 import { errorInterceptor } from './core/interceptors/error';
+import { TranslocoHttpLoader } from './core/transloco-loader';
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -15,5 +18,16 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes, withComponentInputBinding(), withViewTransitions()),
     provideHttpClient(withInterceptors([errorInterceptor])),
     provideAnimationsAsync(),
-  ]
+    provideTransloco({
+      config: {
+        availableLangs: ['en', 'ar', 'de', 'fr', 'es', 'it', 'nl', 'tr'],
+        defaultLang: 'en',
+        fallbackLang: 'en',
+        reRenderOnLangChange: true,
+        prodMode: environment.production,
+      },
+      loader: TranslocoHttpLoader,
+    }),
+  ],
 };
+
